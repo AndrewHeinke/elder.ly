17a7e591efae6f1d98fc816d3ddd3a2dbe3ff030
Andrew <andrew.j.heinke@gmail.com>
2016-09-21 21:04:58 -0500

    updating user schema

--- app/models/user.js
+++ app/models/user.js
@@ -3,12 +3,31 @@ var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema ({
   local: {
-    firstName: String,
-    lastName: String,
-    dob: Date,
-    joinedOn: { type: Date, default: Date.now },
-    email: String,
-    password: String,
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    joinedOn: {
+      type: Date,
+      default: Date.now,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
   }
 });
 
