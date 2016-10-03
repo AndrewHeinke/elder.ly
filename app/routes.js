a576e58317f5a3d323dc4e37fe212ee4da2f363e
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-10-02 20:06:38 -0500

    added template for individual user page, attached document object id to each helper that shows up when on search page, need to build specific user route

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, passport) {
 
   app.get('/', function(req, res) {
     res.render('pages/index', {
-      user: req.user
+      user: req.user,
     });
   });
 
@@ -25,13 +25,31 @@ module.exports = function(app, passport) {
   app.get('/search', isLoggedIn, function(req, res) {
     var userState = req.user.local.state;
     var userCity = req.user.local.city;
-    // want to query the database so it will only return users who are volunteers and are in the same city and state as the member trying to access the search page
-    User.find({'local.userType' : 'volunteer', 'local.state' : userState, 'local.city' : userCity}, function(err, doc) {
+    var userID = req.user._id;
+    // want to query the database so it will only return users who are volunteers and are in the same city and state as the member trying to access the search page. Will exclude the user from showing up if the user is a volunteer
+    User.find({'local.userType' : 'volunteer', 'local.state' : userState, 'local.city' : userCity, '_id': { $nin: userID }}, function(err, doc) {
       if (err) {
         res.send(err);
       }
       else {
+        console.log(doc);
         res.render('pages/search', {
+          list: doc,
+          user: req.user
+        });
+      }
+    });
+  });
+
+  // Get a specifc profile page for a user (volunteer)
+  app.get('/user/:id', isLoggedIn, function(req, res) {
+    User.findOne({'_id': mongojs.ObjectId(req.params.id)}, function(err, doc) {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        console.log(doc);
+        res.render('pages/user', {
           list: doc,
           user: req.user
         });
